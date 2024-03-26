79f4a3d36c596e28e878a1e327a4dc8f010f0369
Bruno Trigueiro <bruno.trigueiro@outlook.com>
2024-03-26 18:12:51 -0300

    Base do projeto - inicialização do repositório
    Arquivos base do NEXTJS + pseudo-documentação geral sobre o funcionamento de partes do projeto.

---
+++ pagamento.lol/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+//  Página da loja. Apenas disponível depois de passar por login/login.js
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    </main>
+  );
+}
